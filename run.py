43ee21ff4e999f22c3d46acacd8bb8268fee240e
Hasmelash95 <hermonasmelash@yahoo.com>
2022-08-16 14:38:30 +0000

    Offer two options for iron guard game

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import os
 # For slow print
 import sys
 import time
+# For dice rolls
+from random import randint
 
 # Dictionary to track the scores
 score = {"iron_guard": 0, "smiling_shadows": 0}
@@ -26,7 +28,7 @@ def main():
     while True:
         user_input = input("Would you like to start the game?" 
                            " Choose A or B:\nA) Yes\nB) No\n"
-                           "You can exit anytime by typing 'exit'").strip().lower()
+                           "You can exit anytime by typing 'exit'\n").lower()
         if user_input == "a":
             get_username()
             break
@@ -46,7 +48,7 @@ def get_username():
     global username
     while True:
         username = input("What's your name?"
-                         " Choose a name with 1-8 chars:\n").strip()
+                         " Choose a name with 1-8 chars:\n")
         if len(username) >= 1 and len(username) <= 8:
             choose_org()  
             break
@@ -63,7 +65,7 @@ def choose_org():
         org = input("Are you an Iron Guard or a Smiling Shadow?\n"
                     " Choose A or B:\n"
                     " A: Iron Guard\n"
-                    " B: Smiling Shadow\n")
+                    " B: Smiling Shadow\n").lower()
         if org == "a":
             slow_print(f"Hello {username}, the citizens of Arx once" 
                        " again need you to keep them safe. There are"
@@ -90,7 +92,25 @@ def choose_org():
             slow_print("Please choose A or B")   
 
 def good_guy():
-    print("Congrats")
+    investigation_style = input("Do you gently question the commoners"
+                                " in the Lower Boroughs?"
+                                " Choose A or B:\n"
+                                " A: Of course!\n"
+                                " B: No, I rough them up a bit.\n").lower()
+    if investigation_style == "a":
+        good_guy_two()
+    elif investigation_style == "b":
+        dice_roll()
+    elif investigation_style == "exit":   
+        main()
+    else:
+        slow_print("Please choose A or B") 
+
+def dice_roll():
+    print("roollll")
+
+def good_guy_two():
+    print("yaaay")
 
 def bad_guy():
     print("mwahaha")
