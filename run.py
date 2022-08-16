0dd04cc7291c61b1b8c0b55f71e11f40b59f0d04
Hasmelash95 <hermonasmelash@yahoo.com>
2022-08-16 13:07:01 +0000

    Define username function

--- run.py
+++ run.py
@@ -20,14 +20,34 @@ def slow_print(text):
 
 
 def main():
-    user_input = input("Would you like to start the game?" 
-               "(Choose a or b):\na) Yes\nb) No\n").strip().lower()
-    if user_input == "a":
-        slow_print("yay")
-    if user_input == "b":
-        slow_print("awww")
+    """
+    Function defining main starting point
+    """
+    while True:
+        user_input = input("Would you like to start the game?" 
+               " Choose A or B:\nA) Yes\nB) No\n").strip().lower()
+        if user_input == "a":
+            get_username()
+            break
+        elif user_input == "b":
+            slow_print("awww")
+            break
+        else:
+            slow_print("Please choose A or B")     
+            
 
+def get_username():
+    """
+    Function to define what a valid username would be
+    """
+    while True:
+        username = input("What's your name?"
+                        " Choose a name with 1-8 chars:\n").strip()
+        if len(username) >= 1 and len(username) <= 8:
+            break
+        else:
+            slow_print("Invalid input, username must be 1-8 chars long.\n")
+        
+    return username
 
-main()
-
-
+main()   
