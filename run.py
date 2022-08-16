a00a0dd33d028eb9ee1ba6649ff618354d840a23
Hasmelash95 <hermonasmelash@yahoo.com>
2022-08-16 10:31:19 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Importing modules
+# To use os.clear to clear the terminal
+import os 
+# For slow print
+import sys
+import time
