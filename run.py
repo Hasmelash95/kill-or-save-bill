bba60d76fe212870493ccb8662571be1b287f33c
Hasmelash95 <hermonasmelash@yahoo.com>
2022-08-16 12:07:12 +0000

    Remove function and put contents into main

--- run.py
+++ run.py
@@ -10,19 +10,24 @@ score = {"iron_guard": 0, "smiling_shadows": 0}
 
 def slow_print(text):
     """
-    Function to define how the output is printed to the user. Code credit to Stack Overflow.
+    Function to define how the output is printed to the user. 
+    Code credit to Stack Overflow.
     """
     for letter in text:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(0.07)
 
-def user_prompt(prompt):
-    """
-    Function to format the user response
-    """
 
-    user_input = input(prompt + "\n").strip().lower()
-    return user_input
+def main():
+    user_input = input("Would you like to start the game?" 
+               "(Choose a or b):\na) Yes\nb) No\n").strip().lower()
+    if user_input == "a":
+        slow_print("yay")
+    if user_input == "b":
+        slow_print("awww")
 
-def 
+
+main()
+
+
